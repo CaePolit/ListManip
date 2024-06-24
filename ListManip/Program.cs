5d8cc9033b5a24c167fd867b1dc251104683d633
CaePolit <caepolit@gmail.com>
2024-06-24 18:26:51 +0200

    Ajoutez des fichiers projet.

---
+++ ListManip/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        //Console.Write("premier chaine: ");
+        List<string> listeChaines = new List<string>(3);
+        listeChaines.Add(" uno ");
+        listeChaines.Add(" dos ");
+        listeChaines.Add(" tres ");
+        foreach (string s in listeChaines) { Console.WriteLine(s); }
+        Console.WriteLine("---------------------------------");
+        listeChaines.RemoveAt(0);
+        foreach (string s in listeChaines) { Console.WriteLine(s); }
+        Console.WriteLine("---------------------------------");
+        listeChaines.Remove(" tres ");
+        foreach (string s in listeChaines) { Console.WriteLine(s); }
+        Console.WriteLine("---------------------------------");
+        listeChaines[0] = " nuevo uno ";
+        foreach (string s in listeChaines) { Console.WriteLine(s); }
+        Console.WriteLine("---------------------------------");
+
+    }
+}
